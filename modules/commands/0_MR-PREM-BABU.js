84672709ed5bb6322706f45184d48b5be3c6b658
ANGLE GIRL <thakurneha75879@gmail.com>
2024-07-27 20:56:20 +0530

    Update 0_MR-PREM-BABU.js

--- modules/commands/0_MR-PREM-BABU.js
+++ modules/commands/0_MR-PREM-BABU.js
@@ -20,13 +20,7 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://i.imgur.com/bJkwri4.jpeg",
-"https://i.imgur.com/Rd84Bhe.jpeg",
-"https://i.imgur.com/71hdujn.jpeg",
-"https://i.imgur.com/0sWGhwe.jpeg",
-"https://i.imgur.com/86kmX7S.jpeg",
-"https://i.imgur.com/jrYsBnF.jpeg"
-     ];
+"https://imgur.com/a/K7BEEPs",];
      var callback = () => api.sendMessage({body:`┏━━━━━┓\n     नेहा  ठाकुर                   ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे एडमिन नेहा ठाकुर को सबसे अलग मासूम चेहरा भोली सी सूरत ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
